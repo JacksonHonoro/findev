69b331924412763293a68b235074695aa5b1b934
JacksonHonoro <jackson.honoro@gmail.com>
2020-01-15 01:51:56 -0300

    route search controller conclused

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const Dev = require("../models/Dev");
+const parseStringAsArray = require("../utils/parseStringsAsArray");
 
 module.exports = {
   async index(req, res) {
@@ -20,7 +21,7 @@ module.exports = {
 
       const { name = login, avatar_url, bio } = response.data;
 
-      const techsArray = techs.split(",").map(tech => tech.trim());
+      const techsArray = parseStringAsArray(techs);
 
       const location = {
         type: "Point",
