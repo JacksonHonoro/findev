420ac66488ab950d39a6dfc8e4e166bdc1242b63
JacksonHonoro <jackson.honoro@gmail.com>
2020-01-16 14:56:47 -0300

    routes completed

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 
 import Routes from './src/routes';
 
 const App: () => React$Node = () => {
   return (
     <>
+      <StatusBar barStyle="light-content" backgroundColor="#005d93" />
       <Routes />
     </>
   );
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -7,17 +7,25 @@ import Profile from './pages/Profile';
 const Routes = createAppContainer(
   createStackNavigator(
     {
-      Main,
-      Profile,
+      Main: {
+        screen: Main,
+        navigationOptions: {
+          title: 'DevRadar',
+        },
+      },
+      Profile: {
+        screen: Profile,
+        navigationOptions: {
+          title: 'Perfil GitHub',
+        },
+      },
     },
     {
-      headerLayoutPreset: 'center',
-      headerBackTitleVisible: false,
       defaultNavigationOptions: {
+        headerTintColor: '#FFF',
         headerStyle: {
           backgroundColor: '#005d93',
         },
-        headerTintColor: '#FFF',
       },
     },
   ),
